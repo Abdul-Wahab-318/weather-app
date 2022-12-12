7cb4e75dc67340480a74f4d68796ab20b8bb5626
Abdul-Wahab-318 <wahabmaliq@gmail.com>
2022-12-12 19:38:47 +0500

    first commit 0_0

---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React , { useState } from 'react'
+import './Home.css'
+export default function Home() {
+  return (
+    <main className='home-component'>
+        <div className="home-component-inner">
+            
+        </div>
+    </main>
+  )
+}
